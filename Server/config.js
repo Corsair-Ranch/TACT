1fad7dc06e5db33c55bd70450d9d5302feb6fd9d
d.sauerbronn <linkdayan2000@gmail.com>
2023-08-05 15:24:02 -0500

    add optional env value for retries for migrations and seed. default is 10

--- Server/config.js
+++ Server/config.js
@@ -1,12 +1,17 @@
-// const dotenv = require('dotenv');
-// dotenv.config();
-
 import "dotenv/config";
 
-// // Exporting env variable
-// module.exports = {
+// Exporting env variable
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-// };
+const SERVER_MIGRATION_RETRIES =
+  process.env.SERVER_MIGRATION_RETRIES === undefined
+    ? 10
+    : process.env.SERVER_MIGRATION_RETRIES;
 
-export { CLIENT_ID, CLIENT_SECRET };
+if (CLIENT_ID === undefined || CLIENT_SECRET === undefined) {
+  throw new Error(
+    `Please edit/create .env file in Server root and add the Amadeus config`
+  );
+}
+
+export { CLIENT_ID, CLIENT_SECRET, SERVER_MIGRATION_RETRIES };
